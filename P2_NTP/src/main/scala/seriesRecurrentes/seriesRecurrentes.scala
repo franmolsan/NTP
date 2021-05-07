343a831d2e161c6ab6b7dbe286a71fab0821577c
franmolsan <56502397+franmolsan@users.noreply.github.com>
2021-05-07 13:13:59 +0200

    arregladas series y comentarios

--- P2_NTP/src/main/scala/seriesRecurrentes/seriesRecurrentes.scala
+++ P2_NTP/src/main/scala/seriesRecurrentes/seriesRecurrentes.scala
@@ -2,56 +2,66 @@ package seriesRecurrentes
 
 object seriesRecurrentes {
 
-
   /**
    * Función genérica para generar una serie
    * @param generadorSiguiente la función que especifica como se obtiene el siguiente término de la serie
-   * @param serie array con los elementos de la serie generados hasta el momento
-   * @param tope límite en el que se para de generar la serie
-   * @return la serie, en forma de array
+   * @param n el término que queremos generar de la serie (empezando desde el 1)
+   * @param primero el primer término de la serie
+   * @param segundo el segundo término de la serie
+   * @return el término n de la serie (empezando desde 1)
    */
   def calcularSerie (generadorSiguiente: (Int, Int) => Int)(n: Int, primero: Int, segundo: Int) : Int = {
 
     /**
      * Función interna tail recursive
-     * Gestiona el acumulador de la recursividad, por lo que
-     * permite que la función general ("generadorSerie") tenga una interfaz más natural
-     * @param serie
-     * @param acum
+     * @param n Int que nos sirve para parar la recursividad (cuando llegue a 0)
+     *          Lo vamos disminuyendo en cada recursión
+     * @param previo El término generado en la llamada anterior
+     * @param actual El término generado en la llamada actual
      * @return
      */
     @annotation.tailrec
     def go (n: Int, previo: Int, actual: Int): Int ={
 
-      // si hemos calculado el elemento que nos han pedido, devolverlo
+      // si hemos calculado el término que nos han pedido, devolverlo
       if (n == 0) actual
-      // si no, seguir la recursividad.
+      // si no, seguir la recursividad
       else {
         // generamos el siguiente término pasándole los dos anteriores
         val nuevoDato : Int = generadorSiguiente(previo,actual)
 
-        // recursividad, aumentando el acumulador y añadiendo el térrmino generado a la serie
+        // recursividad, aumentando el acumulador y añadiendo el término generado a la serie
         go(n-1,actual,nuevoDato)
       }
     }
 
-    //desencadenar la recursividad
-    go(n,segundo,primero)
+    // desencadenar la recursividad
+    // pasamos n-2 porque ya tenemos los dos primeros términos
+    if (n > 2){
+      go(n-2,primero,segundo)
+    }
+    // si nos piden el término 2 o el termino 1, no tenemos que calcularlos
+    else if (n == 2) segundo
+    else if (n == 1) primero
+
+    // si nos piden el término 0 o negativo, devolvemos -1
+    // para indicar que no es correcto
+    else -1
   }
 
   /**
    * Función específica para generar la serie de Fibonacci
    * Se especifican los términos iniciales
    * Y la función con la que conseguimos los términos sucesivos
-   * @param tope Límite en el que paramos de generar la serie, tiene como valor por defecto 10
-   * @return la serie de Fibonacci en forma de Array.
+   * @param n Límite en el que paramos de generar la serie, tiene como valor por defecto 10
+   *          Empieza en 1 (es decir, que el primer elemento de la serie es el 1, no el 0)
+   * @return El elemento n de la serie de Fibonacci (si n<1, se devolverá -1)
    */
-  def serieFibonacci (tope : Int = 10) : Array[Int] = {
+  def serieFibonacci (n : Int = 10) : Int = {
 
-    // generamos la serie con los dos términos iniciales
+    // especificamos los dos términos iniciales
     val primerTermino = 0
     val segundoTermino = 1
-    val serie : Array[Int] = Array(primerTermino,segundoTermino)
 
     // en este caso, el término siguiente se obtiene mediante la suma de los dos anteriores
     val funcionFibonacci : (Int, Int) => Int = _+_
@@ -59,25 +69,25 @@ object seriesRecurrentes {
     // generamos función específica
     // a partir de la función genérica,
     // especificando la forma de obtener el término siguiente (la suma)
-    val generadorFibonacci: (Array[Int], Int) => Array[Int] = generadorSerie(funcionFibonacci)_
+    val generadorFibonacci: (Int,Int, Int) => Int = calcularSerie(funcionFibonacci)_
 
     // llamamos a la función específica que genera la serie de Fibonacci
-    generadorFibonacci(serie, tope)
+    generadorFibonacci(n,primerTermino,segundoTermino)
   }
 
   /**
    * Función específica para generar la serie de Lucas
    * Se especifican los términos iniciales
    * Y la función con la que conseguimos los términos sucesivos
-   * @param tope Límite en el que paramos de generar la serie, tiene como valor por defecto 10
-   * @return la serie de Lucas en forma de Array.
+   * @param n Límite en el que paramos de generar la serie, tiene como valor por defecto 10
+   *          Empieza en 1 (es decir, que el primer elemento de la serie es el 1, no el 0)
+   * @return El elemento n de la serie de Lucas (si n<1, se devolverá -1)
    */
-  def serieLucas (tope : Int = 10) : Array[Int] = {
+  def serieLucas (n : Int = 10) : Int = {
 
     // generamos la serie con los dos términos iniciales
     val primerTermino = 2
     val segundoTermino = 1
-    val serie : Array[Int] = Array(primerTermino,segundoTermino)
 
     // en este caso, el término siguiente se obtiene mediante la suma de los dos anteriores
     val funcionLucas : (Int, Int) => Int = _+_
@@ -85,25 +95,25 @@ object seriesRecurrentes {
     // generamos función específica
     // a partir de la función genérica,
     // especificando la forma de obtener el término siguiente (la suma)
-    val generadorLucas = generadorSerie(funcionLucas)_
+    val generadorLucas = calcularSerie(funcionLucas)_
 
     // llamamos a la función específica que genera la serie de Lucas
-    generadorLucas(serie, tope)
+    generadorLucas(n,primerTermino,segundoTermino)
   }
 
   /**
    * Función específica para generar la serie de Pell
    * Se especifican los términos iniciales
    * Y la función con la que conseguimos los términos sucesivos
-   * @param tope Límite en el que paramos de generar la serie, tiene como valor por defecto 10
-   * @return la serie de Pell en forma de Array.
+   * @param n Límite en el que paramos de generar la serie, tiene como valor por defecto 10
+   *          Empieza en 1 (es decir, que el primer elemento de la serie es el 1, no el 0)
+   * @return El elemento n de la serie de Pell (si n<1, se devolverá -1)
    */
-  def seriePell (tope : Int = 10) : Array[Int] = {
+  def seriePell (n : Int = 10) : Int = {
 
     // generamos la serie con los dos términos iniciales
     val primerTermino = 2
     val segundoTermino = 6
-    val serie : Array[Int] = Array(primerTermino,segundoTermino)
 
     // función para conseguir el siguiente término a partir de los dos anteriores
     val funcionPell : (Int, Int) => Int = _+2*_
@@ -111,25 +121,25 @@ object seriesRecurrentes {
     // generamos función específica
     // a partir de la función genérica,
     // especificando la forma de obtener el término siguiente
-    val generadorPell = generadorSerie(funcionPell)_
+    val generadorPell = calcularSerie(funcionPell)_
 
     // llamamos a la función específica que genera la serie de Pell
-    generadorPell(serie, tope)
+    generadorPell(n,primerTermino,segundoTermino)
   }
 
   /**
    * Función específica para generar la serie de Pell-Lucas
    * Se especifican los términos iniciales
    * Y la función con la que conseguimos los términos sucesivos
-   * @param tope Límite en el que paramos de generar la serie, tiene como valor por defecto 10
-   * @return la serie de Pell-Lucas en forma de Array.
+   * @param n Límite en el que paramos de generar la serie, tiene como valor por defecto 10
+   *          Empieza en 1 (es decir, que el primer elemento de la serie es el 1, no el 0)
+   * @return El elemento n de la serie de Pell-Lucas (si n<1, se devolverá -1)
    */
-  def seriePellLucas (tope : Int = 10) : Array[Int] = {
+  def seriePellLucas (n : Int = 10) : Int = {
 
     // generamos la serie con los dos términos iniciales
     val primerTermino = 2
     val segundoTermino = 2
-    val serie : Array[Int] = Array(primerTermino,segundoTermino)
 
     // función para conseguir el siguiente término a partir de los dos anteriores
     val funcionPellLucas : (Int, Int) => Int = _+2*_
@@ -137,25 +147,25 @@ object seriesRecurrentes {
     // generamos función específica
     // a partir de la función genérica,
     // especificando la forma de obtener el término siguiente
-    val generadorPellLucas = generadorSerie(funcionPellLucas)_
+    val generadorPellLucas = calcularSerie(funcionPellLucas)_
 
     // llamamos a la función específica que genera la serie de Pell-Lucas
-    generadorPellLucas(serie, tope)
+    generadorPellLucas(n,primerTermino,segundoTermino)
   }
 
   /**
    * Función específica para generar la serie de Jacobsthal
    * Se especifican los términos iniciales
    * Y la función con la que conseguimos los términos sucesivos
-   * @param tope Límite en el que paramos de generar la serie, tiene como valor por defecto 10
-   * @return la serie de Jacobsthal en forma de Array.
+   * @param n Límite en el que paramos de generar la serie, tiene como valor por defecto 10
+   *          Empieza en 1 (es decir, que el primer elemento de la serie es el 1, no el 0)
+   * @return El elemento n de la serie de Jacobsthal (si n<1, se devolverá -1)
    */
-  def serieJacobsthal (tope : Int = 10) : Array[Int] = {
+  def serieJacobsthal (n : Int = 10) : Int = {
 
     // generamos la serie con los dos términos iniciales
     val primerTermino = 0
     val segundoTermino = 1
-    val serie : Array[Int] = Array(primerTermino,segundoTermino)
 
     // función para conseguir el siguiente término a partir de los dos anteriores
     val funcionJacobsthal : (Int, Int) => Int = 2*_+_
@@ -163,10 +173,10 @@ object seriesRecurrentes {
     // generamos función específica
     // a partir de la función genérica,
     // especificando la forma de obtener el término siguiente
-    val generadorJacobsthal = generadorSerie(funcionJacobsthal)_
+    val generadorJacobsthal = calcularSerie(funcionJacobsthal)_
 
     // llamamos a la función específica que genera la serie de Pell
-    generadorJacobsthal(serie, tope)
+    generadorJacobsthal(n,primerTermino,segundoTermino)
   }
 
   /***
@@ -176,10 +186,12 @@ object seriesRecurrentes {
    */
   def main(args: Array[String]) ={
     println("Series definidas de forma recurrente")
-    println("Fibonacci: " + serieFibonacci(15).mkString(" "))
-    println("Lucas: " + serieLucas(15).mkString(" "))
-    println("Pell: " + seriePell(15).mkString(" "))
-    println("Pell-Lucas: " + seriePellLucas(15).mkString(" "))
-    println("Jacobsthal: " + serieJacobsthal(15).mkString(" "))
+
+    val n : Int = 5;
+    println(s"Termino $n de la serie de Fibonacci: ${serieFibonacci(n)}")
+    println(s"Termino $n de la serie de Lucas: ${serieLucas(n)}")
+    println(s"Termino $n de la serie de Pell: ${seriePell(n)}")
+    println(s"Termino $n de la serie de Pell-Lucas: ${seriePellLucas(n)}")
+    println(s"Termino $n de la serie de Jacobsthal: ${serieJacobsthal(n)}")
   }
 }
