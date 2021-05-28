7410db82365302db6e826c2ce573055131c69add
franmolsan <56502397+franmolsan@users.noreply.github.com>
2021-05-28 11:02:06 +0200

    comentado y tests

--- NTP_P3_FranciscoJoseMolinaSanchez/src/main/scala/listaPropia/Lista.scala
+++ NTP_P3_FranciscoJoseMolinaSanchez/src/main/scala/listaPropia/Lista.scala
@@ -20,6 +20,8 @@ case object Nil extends Lista[Nothing]
  */
 case class Cons[+A](cabeza : A, cola : Lista[A]) extends Lista[A]
 
+// las clases case ya están definidas
+// por tanto podemos implementar el objeto Lista
 object Lista extends App {
 
   /**
@@ -30,7 +32,13 @@ object Lista extends App {
    */
   def apply[A](elementos : A*) : Lista[A] = {
 
+    // si no nos quedan elementos por añadir a la lista,
+    // devolvemos una lista vacía que indica el final
     if(elementos.length == 0) Nil
+
+    // si quedan elementos a añadir, llamar a Cons
+    // que creará el nodo de la lista con el elemento correspondiente
+    // y seguir llamando recursivamente a Apply, para crear la cola
     else Cons(elementos.head, Lista(elementos.tail:_*))
   }
 
@@ -42,13 +50,25 @@ object Lista extends App {
    */
   def longitud[A](lista : Lista[A]) : Int = {
 
+    /**
+     * Función interna para calcular la longitud de una lista
+     * @param listaActual la lista que queda por procesar
+     * @param longActual la longitud de la lista que ya hemos procesado
+     * @return la longitud total de la lista
+     */
     @annotation.tailrec
     def go(listaActual: Lista[A], longActual: Int): Int = {
       listaActual match {
+        // si estamos al final de la lista,
+        // devolvemos la longitud que teníamos acumulada
         case Nil => longActual
+        // si nos quedan elementos por procesar,
+        // llamar recursivamente a go y aumentar la longitud acumulada
         case Cons(cabeza,cola) => go(cola,longActual+1)
       }
     }
+
+    // llamada inicial para desencadenar la recursividad
     go (lista,0)
   }
 
@@ -59,13 +79,27 @@ object Lista extends App {
    */
   def sumaEnteros(enteros : Lista[Int]) : Double = {
 
+    /**
+     * Función interna para calcular la suma
+     * de todos los elementos de una lista de enteros
+     * @param enterosRestantes la lista de enteros que no hemos sumado aún
+     * @param sumaActual el acumulador donde guardamos la suma calculada hasta el momento
+     * @return la suma de todos los elementos de una lista de enteros
+     */
     @annotation.tailrec
     def go(enterosRestantes: Lista[Int], sumaActual: Double): Double = {
       enterosRestantes match {
+        // si estamos al final de la lista,
+        // devolver la suma actual, que será la suma de todos
         case Nil => sumaActual
+        // si no estamos al final de la lista,
+        // llamar recursivamente a go pasandole la cola como lista restante
+        // y sumar el elemento de la lista con la suma acumulada
         case Cons(cabeza,cola) => go(cola,sumaActual+cabeza)
       }
     }
+
+    // llamada inicial para desencadenar la recursividad
     go (enteros,0)
   }
 
@@ -76,13 +110,27 @@ object Lista extends App {
    */
   def productoEnteros(enteros : Lista[Int]) : Double = {
 
+    /**
+     * Función interna para calcular el producto
+     * de todos los elementos de una lista de enteros
+     * @param enterosRestantes la lista de los enteros cuyo producto no hemos procesado aún
+     * @param productoActual  el acumulador donde guardamos el producto calculado hasta el momento
+     * @return el producto de todos los elementos de una lista de enteros
+     */
     @annotation.tailrec
     def go(enterosRestantes: Lista[Int], productoActual: Double): Double = {
       enterosRestantes match {
+        // si estamos al final de la lista,
+        // devolver el producto actual, que será el producto de todos los elementos
         case Nil => productoActual
+        // si no estamos al final de la lista,
+        // llamar recursivamente a go pasandole la cola como lista restante
+        // y seguir multiplicando
         case Cons(cabeza,cola) => go(cola,productoActual*cabeza)
       }
     }
+
+    // llamada inicial para desencadenar la recursividad
     go (enteros,1)
   }
 
@@ -118,9 +166,18 @@ object Lista extends App {
   def foldRight[A, B](lista : Lista[A], neutro : B)(funcion : (A, B) => B): B = {
 
     lista match {
+      // si la lista está vacía, devolver elemento neutro
       case Nil => neutro
-      case Cons(cabeza,cola) => if(longitud(cola) == 0) funcion(cabeza,neutro)
-      else funcion(cabeza,foldRight(cola,neutro)(funcion))
+      // si la lista no está vacía
+      case Cons(cabeza,cola) =>
+        // si la longitud de la cola es 0, es porque estamos en el último elemento
+        // en ese caso, aplicar la función
+        if(longitud(cola) == 0) funcion(cabeza,neutro)
+        // si no estamos en el último elemento, debemos llegar a él
+        // por lo que continuamos la recursión.
+        // Devolvemos el resultado de aplicar la función
+        // al elemento de la lista y al resultado de aplicar la función a los elementos de la cola
+        else funcion(cabeza,foldRight(cola,neutro)(funcion))
     }
 
   }
@@ -131,6 +188,7 @@ object Lista extends App {
    * @return
    */
   def sumaFoldRight(listaEnteros : Lista[Int]) : Double = {
+    // llamada a foldright pasando la suma como función a aplicar
     foldRight(listaEnteros,0.0)(_+_)
   }
 
@@ -140,6 +198,7 @@ object Lista extends App {
    * @return
    */
   def productoFoldRight(listaEnteros : Lista[Int]) : Double = {
+    // llamada a foldright pasando el producto como función a aplicar
     foldRight(listaEnteros,1.0)(_*_)
   }
 
@@ -154,7 +213,12 @@ object Lista extends App {
    */
   def asignarCabeza[A](lista : Lista[A], cabezaNueva : A) : Lista[A] = {
     lista match {
+      // si la lista está vacía, devolvemos una lista
+      // con el elemento que nos han pasado como nueva cabeza
       case Nil => Lista(cabezaNueva)
+      // si la lista no está vacía,
+      // devolver la lista con la misma cola pero cambiando la cabeza
+      // por la nueva
       case Cons(cabeza,cola) => Cons(cabezaNueva,cola)
     }
   }
@@ -167,7 +231,10 @@ object Lista extends App {
    */
   def tail[A](lista : Lista[A]): Lista[A] = {
     lista match {
+      // si la lista está vacía
+      // devolvemos una lista vacía también
       case Nil => Nil
+      // si la lista no está vacía, devolvemos la cola
       case Cons(cabeza,cola) => cola
     }
   }
@@ -180,9 +247,15 @@ object Lista extends App {
    * @return
    */
   def eliminar[A](lista : Lista[A], n: Int) : Lista[A] = {
+    // si hemos llegado a n, devolver la lista como nos haya quedado
     if (n == 0) lista
+    // si no hemos llegado a n
     else lista match {
+      // si la lista está vacía, devolverla
       case Nil => lista
+      // si la lista no está vacía,
+      // llamamos recurisvamente a eliminar,
+      // pasándole la cola y disminuyendo n en 1
       case Cons(cabeza,cola) => eliminar(cola,n-1)
     }
   }
@@ -197,9 +270,14 @@ object Lista extends App {
    */
   def eliminarMientras[A](lista : Lista[A], criterio: A => Boolean) : Lista[A] = {
     lista match {
+        // si la lista está vacía, devolverla
       case Nil => lista
+        // si la lista no está vacía
       case Cons(cabeza,cola) =>
+        // si se cumple el criterio, llamar recursivamente a eliminarMientras
+        // pasándole la cola (se ha eliminado el elemento de la cabeza)
         if (criterio(cabeza)) eliminarMientras(cola,criterio)
+        // si no se cumple el cirterio, devolver la lista
         else lista
     }
 
@@ -214,17 +292,33 @@ object Lista extends App {
    */
   def eliminarUltimo[A](lista : Lista[A]) : Lista[A] = {
 
+    /**
+     * Función interna para procesar la lista
+     * @param listaRestante la lista que nos queda por procesar
+     * @param listaActual la lista que ya hemos procesado
+     * @tparam A
+     * @return la lista que nos han pasado inicialmente con todos los elementos menos el último
+     */
     @annotation.tailrec
     def go[A](listaRestante : Lista[A], listaActual: Lista[A]) : Lista[A] = {
       listaRestante match {
-        case Cons(cabeza,cola) => if (longitud(cola) == 0) listaActual
-        else {
-          go(cola, concatenar(listaActual,Lista(cabeza)))
-        }
+          // si la lista es vacía, devolverla
+        case Nil => listaRestante
+          // si la lista no está vacía
+        case Cons(cabeza,cola) =>
+          // si la longitud de la cola es 0, es porque estamos en el último elemento
+          // entonces, devolver la listaActual
+          // (que contiene todos los elementos menos este último)
+          if (longitud(cola) == 0) listaActual
+          // si no estamos en el último elemento,
+          // añadir a la lista actual el elemento que hemos procesado
+          // y llamar a go para seguir procesando la cola
+          else  go(cola, concatenar(listaActual,Lista(cabeza)))
       }
-
     }
-    go(lista, Lista())
+
+    // llamada para desencadenar la recursión
+    go(lista, Nil)
   }
 
   /**
@@ -240,7 +334,13 @@ object Lista extends App {
   @annotation.tailrec
   def foldLeft[A, B](lista : Lista[A], neutro: B)(funcion : (B, A) => B): B = {
     lista match {
+      // si la lista está vacía, devolver neutro
       case Nil => neutro
+      // si quedan elementos en la lista, llamar a foldLeft
+      // pasando como argumentos la cola, ya que tenemos que procesarla,
+      // y como neutro el resultado de aplicar la función al elemento actual de la lista
+      // de esta forma, neutro va actuando como acumulador
+      // y se devolverá cuando llegemos al final
       case Cons(cabeza,cola) => foldLeft(cola,funcion(neutro,cabeza))(funcion)
     }
   }
@@ -278,4 +378,5 @@ object Lista extends App {
 
   println("eliminar ultimo: " + eliminarUltimo(Lista1))
   println("eliminar ultimo: " + eliminarUltimo(concatenar(Lista1,Lista2)))
+  println("eliminar último nil " + eliminarUltimo(Nil))
 }
---
+++ NTP_P3_FranciscoJoseMolinaSanchez/src/test/scala/listaPropia/ListaTest.scala
@@ -0,0 +1,92 @@
+package listaPropia
+
+import listaPropia.Lista.{asignarCabeza, concatenar, eliminar, eliminarMientras, eliminarUltimo, foldLeft, longitud, productoEnteros, productoFoldRight, sumaEnteros, sumaFoldRight, tail}
+import org.scalatest.funsuite.AnyFunSuite
+
+class ListaTest extends AnyFunSuite{
+
+  // variables a utilizar durante las pruebas
+  val ListaEnterosPropia = Lista(1,2,3,4,5)
+  val ListaEnterosScala = List(1,2,3,4,5)
+
+  val ListaCharPropia1 = Lista('a','b','c','d')
+  val ListaCharPropia2 = Lista('e','f','g','h')
+
+  val ListaCharScala1 = List('a','b','c','d')
+  val ListaCharScala2 = List('e','f','g','h')
+
+
+  // prueba 1
+  test("Calculo correcto de la longitud de la lista") {
+    assert(longitud(ListaEnterosPropia) == ListaEnterosScala.length)
+  }
+
+  // prueba 2
+  test("Suma correcta de los elementos de la lista de enteros") {
+    assert(sumaEnteros(ListaEnterosPropia) == ListaEnterosScala.sum)
+  }
+
+  // prueba 3
+  test("Producto correcta de los elementos de la lista de enteros") {
+    assert(productoEnteros(ListaEnterosPropia) == ListaEnterosScala.product)
+  }
+
+  // prueba 4
+  test("Concatenación produce longitud correcta") {
+    // comparamos que el resultado de concatenar tiene la misma longitud
+    // en ambos casos
+    assert(longitud(concatenar(ListaCharPropia1,ListaCharPropia2)) ==
+      (ListaCharScala1 ::: ListaCharScala2).length)
+  }
+
+  // prueba 5
+  test("Concatenación es correcta") {
+    // comprobamos si la concatenación produce el resultado
+    // que debería
+    assert(concatenar(ListaCharPropia1,ListaCharPropia2) ==
+    Lista('a','b','c','d','e','f','g','h'))
+  }
+
+  // prueba 6
+  test("Suma FoldRight es correcta") {
+    assert(sumaFoldRight(ListaEnterosPropia) == ListaEnterosScala.foldRight(0)(_+_))
+  }
+
+  // prueba 7
+  test("Producto FoldRight es correcto") {
+    assert(productoFoldRight(ListaEnterosPropia) == ListaEnterosScala.foldRight(1)(_*_))
+  }
+
+  // prueba 8
+  test("Asignar cabeza es correcto") {
+    assert(asignarCabeza(ListaCharPropia1,'e') == Lista('e','b','c','d'))
+  }
+
+  // prueba 9
+  test("Tail es correcto") {
+    assert(tail(ListaCharPropia1) == Lista('b','c','d'))
+  }
+
+  // prueba 10
+  test("Eliminar n es correcto") {
+    assert(eliminar(ListaCharPropia1, 2) == Lista('c','d'))
+  }
+
+  // prueba 11
+  test("Eliminar mientras es correcto") {
+    val condicionEliminar: (Int) => Boolean = (num) => num < 4
+    assert(eliminarMientras(ListaEnterosPropia, condicionEliminar) == Lista(4,5))
+  }
+
+  // prueba 12
+  test("Eliminar ultimo es correcto") {
+    assert(eliminarUltimo(ListaEnterosPropia) == Lista(1,2,3,4))
+  }
+
+  // prueba 13
+  test("FoldLeft es correcto") {
+    assert(foldLeft(ListaEnterosPropia,1)(_/_) == ListaEnterosScala.foldLeft(1)(_/_))
+  }
+
+
+}
